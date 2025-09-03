6c8ec055db95a11f7625246c4598e9afc0cf8cd7
Pranavi695 <eluripranavi@gmail.com>
2025-09-03 11:30:02 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,73 @@
+class Lab1P1{
+        public static void main (String...args){
+             byte b = 120;
+             Byte bb = b;
+            //int x = 200;
+            //byte a= x;
+            System.out.println(Byte.MAX_VALUE);
+            System.out.println(bb.MIN_VALUE);
+            System.out.println(bb.SIZE);
+            System.out.println(bb.TYPE);
+            System.out.println(bb.BYTES);
+            System.out.println("..........Integer...........");
+            
+            short s = 15;
+            Short ss =s;     
+            System.out.println(Short.MAX_VALUE);
+            System.out.println(ss.MIN_VALUE);
+            System.out.println(ss.SIZE);
+            System.out.println(ss.TYPE);
+            System.out.println(ss.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            
+            long l = 200;
+            Long ll = l;
+            System.out.println(Long.MAX_VALUE);
+            System.out.println(ll.MIN_VALUE);
+            System.out.println(ll.SIZE);
+            System.out.println(ll.TYPE);
+            System.out.println(ll.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            float f = 200;
+            Float ff = f;
+            System.out.println(Float.MAX_VALUE);
+            System.out.println(ff.MIN_VALUE);
+            System.out.println(ff.SIZE);
+            System.out.println(ff.TYPE);
+            System.out.println(ff.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            double d = 200;
+            ii = i;
+            System.out.println(Double.MAX_VALUE);
+            System.out.println(dd.MIN_VALUE);
+            System.out.println(dd.SIZE);
+            System.out.println(dd.TYPE);
+            System.out.println(dd.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            int i = 200;
+            Integer ii = i;
+            System.out.println(Integer.MAX_VALUE);
+            System.out.println(ii.MIN_VALUE);
+            System.out.println(ii.SIZE);
+            System.out.println(ii.TYPE);
+            System.out.println(ii.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            boolean p = 200;
+            Boolean pp = p;
+            System.out.println(Boolean.MAX_VALUE);
+            System.out.println(pp.MIN_VALUE);
+            System.out.println(pp.SIZE);
+            System.out.println(pp.TYPE);
+            System.out.println(pp.BYTES); 
+            System.out.println("..........Integer...........");
+                 
+            
+            
+        
+        }
+}
