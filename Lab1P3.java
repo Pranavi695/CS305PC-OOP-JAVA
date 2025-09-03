ae514d30e5a8c2dc7595301e65c62cb3faddead3
Pranavi695 <eluripranavi@gmail.com>
2025-09-03 12:37:10 +0530

    Add files via upload

---
+++ Lab1P3.java
@@ -0,0 +1,17 @@
+import java.until.Scanner;
+class Lab1P3{
+      public static void main(String...args){
+       Scanner s = new Scanner(System.in);
+       try{
+              int a = s.nextInt();
+              int b = s.nextLine();
+              //var c a/b;
+              System.out.println(a/b);
+      }catch(exception e){
+       Sytem.out.println(e.getClass());
+       }
+     }
+  }
+      
+
+
