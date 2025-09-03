f98c404567d99d1203da6ff12195543ee02bfcec
Pranavi695 <eluripranavi@gmail.com>
2025-09-03 12:16:54 +0530

    Add files via upload

---
+++ Lab1P2.java
@@ -0,0 +1,11 @@
+class Lab1P2{
+    public static void main (String...args){
+        var a = 10;
+        var b = 60.7892;
+         
+        var c = a+b;
+        System.out.println(a+b); 
+    }
+}
+
+
