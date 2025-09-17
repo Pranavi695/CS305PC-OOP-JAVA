705f386f5802ccf0bf8526e27aeae8b92e51bdc6
Pranavi695 <eluripranavi@gmail.com>
2025-09-17 12:30:16 +0530

    Add files via upload

---
+++ Lab3P3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Lab3P3{
+	public static void main(String...args){
+	Lab3P3 l = new Lab3P3();
+	l.display(); l.display(10); l.display("String"); l.display(10,"String");
+	}
+	public void show(){
+		System.out.println("Method without params");
+	}
+	public void show(int x){
+		System.out.println("Method without params"+x);
+	}
+	public void show(String s){
+		System.out.println("Method without params"+s);
+	}
+	public void show(int x,String s){
+		System.out.println("Method without params:"+ x +" "+s);
+	}
+   }	
