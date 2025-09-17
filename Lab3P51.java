6883363c3c2058a3e87ca8253f7f66f7c3ca364b
Pranavi695 <eluripranavi@gmail.com>
2025-09-17 12:50:13 +0530

    Add files via upload

---
+++ Lab3P51.java
@@ -0,0 +1,14 @@
+class Lab3P51{
+	String name;
+	public void demonstrate(){
+		System.out.println("I am a method in Lab3P51");
+	}
+	public void setName(String s){
+		this.name = s;
+	}
+	public void getName(){
+		return this.name;
+		}
+	
+	}	
+		
