71585c26ce86c74f015e6c56aa6c5f4205192c5e
Pranavi695 <eluripranavi@gmail.com>
2025-09-24 12:42:57 +0530

    Add files via upload

---
+++ StudentMain.java
@@ -0,0 +1,10 @@
+package Student;
+import java.util.Sacnner;
+import java.io.*;
+	class StudentMain implements Operation{
+	static
+	public void register (Student std){
+	System.out.println("========================\n Welcome to SMds\n=========================");
+	System.out.println()
+	
+	public void LogIn
